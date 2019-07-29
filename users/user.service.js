837ff1d12160756e01499fa16dcad5ffc547f322
moshe <moshe.vilner@webintpro.com>
2019-07-29 13:19:00 +0300

    jwt secret moved to be on .env file

--- users/user.service.js
+++ users/user.service.js
@@ -1,5 +1,4 @@
-﻿const config = require('config.json');
-const jwt = require('jsonwebtoken');
+﻿const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const db = require('_helpers/db');
 const User = db.User;
@@ -20,7 +19,7 @@ async function authenticate({ username, password }) {
     const user = await User.findOne({ username });
     if (user && bcrypt.compareSync(password, user.hash)) {
         const { hash, ...userWithoutHash } = user.toObject();
-        const token = jwt.sign({ sub: user.id }, config.secret);
+        const token = jwt.sign({ sub: user.id }, process.env.secret);
         return {
             ...userWithoutHash,
             token
