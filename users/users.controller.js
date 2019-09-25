30bdd3defea98e2e0855901774e2e0744fe551a1
moshev <moshev@welldone-software.com>
2019-09-25 14:25:02 +0300

    feat(seeder): should create 3 users & tasks

--- users/users.controller.js
+++ users/users.controller.js
@@ -1,11 +1,12 @@
 ﻿const express = require('express');
 const router = express.Router();
 const userService = require('./user.service');
+const taskService = require('../tasks/task.service');
 
 // seed stuff
 const Seeder = require('../_helpers/seed/seeder');
 if (process.env.SEED) {
-  const s = new Seeder(userService);
+  const s = new Seeder(userService, taskService);
   s.seedDB();       
 }
 
